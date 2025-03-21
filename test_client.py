c742453f8bdbb7bf99481d7407f5ba40a5c06da7
Zhifeng Jiang <samuelgong2017@gmail.com>
2025-03-21 19:27:50 +0800

    A

--- test_client.py
+++ test_client.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from typing import Optional
 from contextlib import AsyncExitStack
 
@@ -7,6 +8,10 @@ from mcp.client.stdio import stdio_client
 
 from anthropic import Anthropic
 from dotenv import load_dotenv
+
+from openai import OpenAI
+import httpx
+import json
 
 load_dotenv()  # load environment variables from .env
 
@@ -49,6 +54,7 @@ class MCPClient:
 
     async def process_query(self, query: str) -> str:
         """Process a query using Claude and available tools"""
+        model = "ep-20250212105505-5zlbx"
         messages = [
             {
                 "role": "user",
@@ -57,57 +63,124 @@ class MCPClient:
         ]
 
         response = await self.session.list_tools()
+
+        # available_tools = [{
+        #     "name": tool.name,
+        #     "description": tool.description,
+        #     "input_schema": tool.inputSchema
+        # } for tool in response.tools]
+        # reference: https://platform.openai.com/docs/api-reference/chat/create
         available_tools = [{
-            "name": tool.name,
-            "description": tool.description,
-            "input_schema": tool.inputSchema
+            "type": "function",
+            "function": {
+                "name": tool.name,
+                "description": tool.description,
+                "input_schema": tool.inputSchema
+            }
         } for tool in response.tools]
+        # Available tools: [{'type': 'function', 'function': {'name': 'get_alerts', 'description': 'Get weather alerts for a US state.\n\n    Args:\n        state: Two-letter US state code (e.g. CA, NY)\n    ', 'input_schema': {'properties': {'state': {'title': 'State', 'type': 'string'}}, 'required': ['state'], 'title': 'get_alertsArguments', 'type': 'object'}}}, {'type': 'function', 'function': {'name': 'get_forecast', 'description': 'Get weather forecast for a location.\n\n    Args:\n        latitude: Latitude of the location\n        longitude: Longitude of the location\n    ', 'input_schema': {'properties': {'latitude': {'title': 'Latitude', 'type': 'number'}, 'longitude': {'title': 'Longitude', 'type': 'number'}}, 'required': ['latitude', 'longitude'], 'title': 'get_forecastArguments', 'type': 'object'}}}]
 
         # Initial Claude API call
-        print(f"Available tools: {available_tools}")
-        response = self.anthropic.messages.create(
-            model="claude-3-5-sonnet-20241022",
-            max_tokens=1000,
-            messages=messages,
-            tools=available_tools
+        # print(f"\nAvailable tools: {available_tools}")
+        # response = self.anthropic.messages.create(
+        #     model="claude-3-5-sonnet-20241022",
+        #     max_tokens=1000,
+        #     messages=messages,
+        #     tools=available_tools
+        # )
+        endpoint = OpenAI(
+            api_key=os.getenv("ARK_API_KEY"),
+            base_url="https://ark.cn-beijing.volces.com/api/v3",
+            http_client=httpx.Client(
+                verify=False  # important for company use
+            )
         )
-        print(f"Response: {response}")
+        payload = {
+            "model": model,
+            "messages": messages,
+            "tools": available_tools
+        }
+        completion = endpoint.chat.completions.create(**payload)
+        # response = completion.choices[0].message.content
+        # print(f"\nMessage: {completion.choices[0]}")
+        # exit()
+        # print(f"Response: {response}")
 
         # Process response and handle tool calls
         tool_results = []
         final_text = []
+        # for content in response.content:
+        if True:
+            choice = completion.choices[0]
 
-        for content in response.content:
-            if content.type == 'text':
-                final_text.append(content.text)
-            elif content.type == 'tool_use':
-                tool_name = content.name
-                tool_args = content.input
+            # if content.type == 'text':
+            if choice.finish_reason == 'stop':
+                # final_text.append(content.text)
+                final_text.append(choice.message.content)
+            # elif content.type == 'tool_use':
+            elif choice.finish_reason == "tool_calls":
+                # tool_name = content.name
+                # tool_args = content.input
+                message = choice.message
+                tool_call = message.tool_calls[0]
+                tool_call_id = tool_call.id
+                function = tool_call.function
+                tool_name = function.name
+                tool_args = function.arguments
+
+                # necessary for doubao 1.5 which is not fully aligned
+                tool_args = json.loads(tool_args)
 
                 # Execute tool call
-                result = await self.session.call_tool(tool_name, tool_args)
-                tool_results.append({"call": tool_name, "result": result})
+                tool_call_result = await self.session.call_tool(tool_name, tool_args)
+                # print(f"\nResult: {tool_call_result}")
+                # print(type(tool_call_result.content[0]))
+                # exit(0)
+
+                tool_results.append({"call": tool_name, "result": tool_call_result})
                 final_text.append(f"[Calling tool {tool_name} with args {tool_args}]")
 
                 # Continue conversation with tool results
-                if hasattr(content, 'text') and content.text:
-                    messages.append({
-                        "role": "assistant",
-                        "content": content.text
-                    })
+                # if hasattr(content, 'text') and content.text:
+                #     messages.append({
+                #         "role": "assistant",
+                #         "content": content.text
+                #     })
+                # messages.append({
+                #     "role": "user",
+                #     "content": result.content
+                # })
                 messages.append({
-                    "role": "user",
-                    "content": result.content
+                    "role": "assistant",
+                    "content": message.content,
+                    "tool_calls": [{
+                        "function": {"name": tool_name, "arguments": json.dumps(tool_args)},
+                        "id": tool_call_id,
+                        "type": "function"
+                    }]
                 })
 
                 # Get next response from Claude
-                response = self.anthropic.messages.create(
-                    model="claude-3-5-sonnet-20241022",
-                    max_tokens=1000,
-                    messages=messages,
-                )
+                # response = self.anthropic.messages.create(
+                #     model="claude-3-5-sonnet-20241022",
+                #     max_tokens=1000,
+                #     messages=messages,
+                # )
+                # final_text.append(response.content[0].text)
+                messages.append({
+                    "role": "tool",
+                    "name": tool_name,
+                    "tool_call_id": tool_call_id,
+                    "content": tool_call_result.content[0].text
+                })
+                payload = {
+                    "model": model,
+                    "messages": messages,
+                    "tools": available_tools
+                }
+                completion = endpoint.chat.completions.create(**payload)
+                final_text.append(completion.choices[0].message.content)
 
-                final_text.append(response.content[0].text)
 
         return "\n".join(final_text)
 
